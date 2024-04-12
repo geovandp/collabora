0354a3e1d3469a1b83f66b7347a67c972333b09e
geovandp <giovan472@gmail.com>
2024-04-12 23:26:16 +0700

    first commit

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionController extends Controller
+{
+    //
+    function dashboard() {
+        return view("dashboard");
+    }
+
+    function login() {
+
+    }
+}
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #222831;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Collabora</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+          <a class="nav-link" href="#">Event</a>
+          <a class="nav-link" href="#">Login</a>
+          {{-- <a class="nav-link disabled" aria-disabled="true">Disabled</a> --}}
+        </div>
+      </div>
+    </div>
+  </nav>
